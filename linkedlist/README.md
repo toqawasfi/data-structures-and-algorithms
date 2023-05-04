84191d745073172dac0b04fe76624a6e44809cb8
toqawasfi <toqawasfi123@gmail.com>
2023-05-04 10:44:22 +0000

    done

--- linkedlist/README.md
+++ linkedlist/README.md
@@ -1,7 +1,26 @@
-# Linked list
+## Title
+CC5: linked_list
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+[linked_list](./CC5.jpg)
 
 ## Approach & Efficiency
-Using Classes i did this task, so i created 2 classes the first one is linked list where i added all of the methods implemntation and Node class where i added my node implementation.
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+The Time complexity for zip_lists function in the current implementation is O(n), where n is the length of the smaller linked list between l1 and l2. 
+The space complexity is O(1)
 
-# Solution
-just run my code using python3 file_path
+Approach:Define the Node class with an __init__ method that takes a value argument and initializes the value and next attributes.
+
+Define the LinkedList class with an __init__ method that initializes the head attribute to None.
+
+Define the __str__ method for the LinkedList class. Initialize a variable called output to an empty string. Set a variable current to self.head. Use a while loop to iterate through the linked list, appending the value of each node to output followed by an arrow symbol. Update current to the next node in the list at each iteration. Finally, append the string "Null" to output to indicate the end of the list, and return output.
+
+Define the __repr__ method for the LinkedList class. Initialize an empty list called nodes. Set a variable current to self.head. Use a while loop to iterate through the linked list, appending the string representation of each node's value to nodes. Update current to the next node in the list at each iteration. 
+
+Define the insert method for the LinkedList class. Create a new Node instance with the given value and assign it to a variable called node. Set a variable current to self.head. Set self.head to node. Set node.next to current.
+
+Define the includes method for the LinkedList class that takes a value argument. Set a variable current to self.head. Use a while loop to iterate through the linked list. If the value of the current node is equal to the given value, return True. Update current to the next node in the list at each iteration. If the end of the list is reached without finding the value, return False.
+## Solution
+<!-- Show how to run your code, and examples of it in action -->
+To run the code you have to pass two .
+
--- tests/test_linkedlist.py
+++ tests/test_linkedlist.py
@@ -1,90 +1,80 @@
-# import pytest
-# from linkedlist.linkedlist import LinkedList
-# from linkedlist.node1 import Node
+import pytest
+from linkedlist.linkedlist import LinkedList
+from linkedlist.node1 import Node
 
-# def test_empty_ll():
-#     ll = LinkedList()
-#     expected = "Empty LinkeList"
-#     actual = str(ll)
-#     assert expected == actual
+def test_empty_LL():
+    ll = LinkedList()
+
+    actual = repr(ll)
+    expected = "Empty LinkedList"
+    assert actual == expected
 
 
-# # Test insert node
-# def test_insert_node(ll):
+# Test insert node
+def test_insert_node(ll):
     
-#     expected = "{Toqa }->  Null"
-#     actual = str(ll)
-#     assert expected == actual
+    expected = "Toqa -> Null"
+    actual = str(ll)
+    assert expected == actual
 
-# @pytest.fixture
-# def ll():
-#     ll = LinkedList()
-#     ll.insert("Toqa")
-#     return ll
-# def test_empty_LL():
-#     ll = LinkedList()
+def test_insert_multiple_nodes():
+    ll = LinkedList()
 
-#     actual = str(ll)
-#     expected = "LL is Empty"
-#     assert actual == expected
+    ll.insert('a')
+    ll.insert('b')
+    ll.insert('c')
 
+    actual = str(ll)
+    expected = "c -> b -> a -> Null"
+    assert actual == expected
 
-# def test_insert_node():
-#     ll = LinkedList()
-#     ll.insert(9)
+def test_value_in_LL():
+    ll = LinkedList()
+    ll.insert(9)
+    ll.insert(10)
+    ll.insert(11)
 
-#     actual = str(ll)
-#     expected = "9 --> Null"
-#     assert actual == expected
+    actual = ll.includes(10)
+    expected = True
+    assert actual == expected
 
-    
+def test_value_in_LL():
+    ll = LinkedList()
+    ll.insert(9)
+    ll.insert(10)
+    ll.insert(11)
 
-   
-# def test_insert_multiple_nodes():
-#     ll = LinkedList()
-
-#     ll.insert('a')
-#     ll.insert('b')
-#     ll.insert('c')
-
-#     actual = str(ll)
-#     expected = "c --> b --> a --> Null"
-#     assert actual == expected
-
-  
-
-
-
-# def test_value_in_LL():
-#     ll = LinkedList()
-#     ll.insert(9)
-#     ll.insert(10)
-#     ll.insert(11)
-
-#     actual = ll.includes(10)
-#     expected = True
-#     assert actual == expected
+    actual = ll.includes(10)
+    expected = True
+    assert actual == expected
 
     
 
-# def test_value_not_in_LL():
-#     ll = LinkedList()
-#     ll.insert(9)
-#     ll.insert(10)
-#     ll.insert(11)
+def test_value_not_in_LL():
+    ll = LinkedList()
+    ll.insert(9)
+    ll.insert(10)
+    ll.insert(11)
 
-#     actual = ll.includes(5)
-#     expected = False
-#     assert actual == expected
+    actual = ll.includes(5)
+    expected = False
+    assert actual == expected
 
 
 
-# def test_values():
-#     ll = LinkedList()
-#     ll.insert(9)
-#     ll.insert(10)
-#     ll.insert(11)
+def test_values():
+    ll = LinkedList()
+    ll.insert(9)
+    ll.insert(10)
+    ll.insert(11)
 
-#     actual = str(ll)
-#     expected = "11 --> 10 --> 9 --> Null"
-#     assert actual == expected
+    actual = str(ll)
+    expected = "11 -> 10 -> 9 -> Null"
+    assert actual == expected
+    
+
+@pytest.fixture
+def ll():
+    ll = LinkedList()
+    ll.insert("Toqa")
+    return ll
